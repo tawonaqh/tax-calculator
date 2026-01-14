835f9e701bac94ad96a5db8764ded2ad68bb9744
tawona-swe <tnrwatida@gmail.com>
2026-01-14 15:15:48 +0200

    add speed insights component

--- tax-frontend/src/app/layout.js
+++ tax-frontend/src/app/layout.js
@@ -50,6 +50,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <div className="w-full min-h-screen flex flex-col bg-[#EEEEEE] text-[#0F2F4E]">
+          <SpeedInsights />
           <Header />
           {children}
           <Chatbot />
